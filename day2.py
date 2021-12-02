351325227a177e4b0badfad2c3be8fc617e9d33f
michielderoos <michiel@deroos.ca>
2021-12-02 09:22:17 -0400

    Day 2

---
+++ day2.py
@@ -0,0 +1,34 @@
+def part_one(input):
+    horizontal = 0
+    vertical = 0
+    for line in input:
+        direction, distance = line.rstrip().split(' ')
+        distance = int(distance)
+        if direction == 'forward':
+            horizontal+= distance
+        elif direction == 'up':
+            vertical -= distance
+        elif direction == 'down':
+            vertical += distance
+    return horizontal * vertical
+
+def part_two(input):
+    horizontal = 0
+    vertical = 0
+    aim = 0
+    for line in input:
+        direction, distance = line.rstrip().split(' ')
+        distance = int(distance)
+        if direction == 'forward':
+            horizontal+= distance
+            vertical += distance * aim
+        elif direction == 'up':
+            aim -= distance
+        elif direction == 'down':
+            aim += distance
+    return horizontal * vertical
+
+input = open('day2.input.txt', 'r')
+print(f'Part 1: {part_one(input)} is the solution')
+input = open('day2.input.txt', 'r')
+print(f'Part 2: {part_two(input)} is the solution')
